8af264c537f5115375ad76ee9d00c4a557e55a26
UgiR <33397860+UgiR@users.noreply.github.com>
2019-01-02 18:19:57 -0600

    User attributes testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,32 +1,13 @@
 import pytest
 import datetime as dt
-import time
 from uuid import UUID
 from PEC.user.models import User, Role
+from PEC.user.attributes import Role, Course, Skill
 from tests.factories import UserFactory
 
 
 @pytest.mark.usefixtures('db')
 class TestUser:
-    def test_get_by_id(self):
-        user = User('username', 'email@email.com')
-        user.save()
-        retrieved = User.query.get(user.id)
-        assert retrieved == user
-
-    def test_created_at_defaults_to_datetime(self):
-        user = User('username', 'email@email.com')
-        user.save()
-        assert bool(user.created_at)
-        assert isinstance(user.created_at, dt.datetime)
-
-    def test_uuid_defaults_unique(self, db):
-        user1 = UserFactory()
-        user2 = UserFactory()
-        user1.save()
-        user2.save()
-        assert user1.uuid is not user2.uuid
-        assert isinstance(user1.uuid, UUID)
 
     def test_factory(self, db):
         user = UserFactory(password='password123')
@@ -37,6 +18,22 @@ class TestUser:
         assert bool(user.last_name)
         assert user.active is False
         assert user.check_password('password123')
+
+    def test_get_by_id(self, user):
+        retrieved = User.query.get(user.id)
+        assert retrieved == user
+
+    def test_created_at_defaults_to_datetime(self, user):
+        assert bool(user.created_at)
+        assert isinstance(user.created_at, dt.datetime)
+
+    def test_uuid_defaults_unique(self, db):
+        user1 = UserFactory()
+        user2 = UserFactory()
+        user1.save()
+        user2.save()
+        assert user1.uuid is not user2.uuid
+        assert isinstance(user1.uuid, UUID)
 
     def test_check_password(self):
         user = UserFactory(password='password123')
@@ -54,10 +51,27 @@ class TestUser:
         assert user.check_password('password123') is False
         assert user.check_password('different_password') is True
 
-    def test_roles(self):
-        role = Role('admin')
-        role.save()
-        user = UserFactory()
-        user.roles.append(role)
+    def test_roles(self, user):
+        user.add_role(Role.USER, Role.MODERATOR)
         user.save()
-        assert role in user.roles
+        assert user.has_role()
+        assert user.has_role(Role.USER, Role.MODERATOR)
+        assert not user.has_role(Role.USER, Role.ADMIN)
+
+    def test_courses(self, user):
+        assert len(user.courses) == 0
+        user.add_course(Course.CS111, Course.CS141, Course.CS151)
+        user.save()
+        assert user.has_course()
+        assert user.has_course(Course.CS111)
+        assert user.has_course(Course.CS111, Course.CS141, Course.CS151)
+        assert not user.has_course(Course.CS111, Course.CS251)
+
+    def test_skills(self, user):
+        assert len(user.skills) == 0
+        user.add_skill(Skill.PYTHON, Skill.FLASK, Skill.BOOTSTRAP)
+        user.save()
+        assert user.has_skill()
+        assert user.has_skill(Skill.FLASK)
+        assert user.has_skill(Skill.PYTHON, Skill.FLASK, Skill.BOOTSTRAP)
+        assert not user.has_skill(Skill.PYTHON, Skill.JAVASCRIPT)
