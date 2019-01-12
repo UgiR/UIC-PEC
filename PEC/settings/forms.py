16b9894694ae15814f1544d142ef12fce83112c4
UgiR <33397860+UgiR@users.noreply.github.com>
2019-01-12 13:36:55 -0600

    Continued work on Settings blueprint

--- PEC/settings/forms.py
+++ PEC/settings/forms.py
@@ -1,8 +1,10 @@
 from flask_login import current_user
 from flask_wtf import FlaskForm
-from PEC.user.models import User
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectMultipleField
+from wtforms.widgets import CheckboxInput, ListWidget
 from wtforms.validators import Email, Length, DataRequired
+from PEC.user.models import User
+from PEC.user.attributes import Course, Skill
 
 
 class AccountDetailForm(FlaskForm):
@@ -12,6 +14,10 @@ class AccountDetailForm(FlaskForm):
     submit = SubmitField('Update')
 
     def validate(self):
+        """ Checks that updated information does not conflict with other users in DB
+
+        :return: True if updated email is unique, False otherwise
+        """
         initial_validation = super(AccountDetailForm, self).validate()
         if not initial_validation:
             return False
@@ -20,3 +26,15 @@ class AccountDetailForm(FlaskForm):
             return True
         self.email.errors.append('Email address already in use')
         return False
+
+
+class PortfolioForm(FlaskForm):
+    course_selection = SelectMultipleField('Completed Coursework',
+                                           option_widget=CheckboxInput(),
+                                           widget=ListWidget(prefix_label=False),
+                                           choices=Course.to_choices())
+    skill_selection = SelectMultipleField('Skills',
+                                          option_widget=CheckboxInput(),
+                                          widget=ListWidget(prefix_label=False),
+                                          choices=Skill.to_choices())
+    submit = SubmitField('Update')
--- PEC/settings/views.py
+++ PEC/settings/views.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import current_user, login_required
-from PEC.settings.forms import AccountDetailForm
+from PEC.settings.forms import AccountDetailForm, PortfolioForm
+from PEC.user.attributes import Course, Skill
+from PEC.user.models import Course as Course_
+from PEC.user.models import Skill as Skill_
+from PEC.utils import flash_form_errors
 
 blueprint = Blueprint('settings', __name__, static_folder='../static', url_prefix='/settings')
 
@@ -8,6 +12,8 @@ blueprint = Blueprint('settings', __name__, static_folder='../static', url_prefi
 @blueprint.route('/account/details', methods=['GET', 'POST'])
 @login_required
 def account_details():
+    """ Account details settings page
+    """
     detail_form = AccountDetailForm()
     if request.method == 'POST':
         if detail_form.validate_on_submit():
@@ -16,6 +22,33 @@ def account_details():
             flash('Account details updated')
             return redirect(url_for('settings.account_details'))
         else:
-            flash(detail_form.errors)
+            flash_form_errors(detail_form)
             return redirect(url_for('settings.account_details'))
     return render_template('settings/account/details.html', uuid=current_user.uuid, detail_form=detail_form)
+
+
+@blueprint.route('/account/portfolio', methods=['GET', 'POST'])
+@login_required
+def account_portfolio():
+    """ Account portfolio settings page
+    """
+    current_courses = [c.name.value for c in current_user.courses]
+    current_skills = [s.name.value for s in current_user.skills]
+    portfolio_form = PortfolioForm(course_selection=current_courses, skill_selection=current_skills)
+
+    if request.method == 'POST':
+        if portfolio_form.validate_on_submit():
+            current_user.courses.clear()
+            for selected in portfolio_form.course_selection.data:
+                course = Course_.get(name=Course(selected))
+                current_user.courses.append(course)
+            for selected in portfolio_form.skill_selection.data:
+                skill = Skill_.get(name=Skill(selected))
+                current_user.skills.append(skill)
+            current_user.save()
+            return redirect(url_for('settings.account_portfolio'))
+        else:
+            flash_form_errors(portfolio_form)
+            return redirect(url_for('settings.account_portfolio'))
+
+    return render_template('settings/account/portfolio.html', portfolio_form=portfolio_form)
